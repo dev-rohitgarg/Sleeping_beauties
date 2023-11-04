5f07dc588d3946ea4cba0349012e3dcd101323cc
rohit5803 <220010049.iitdh.ac.in>
2023-11-05 04:09:45 +0530

    Django setup

---
+++ Sleeping_beauties/Sleeping_beauties/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Sleeping_beauties project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sleeping_beauties.settings')
+
+application = get_wsgi_application()
