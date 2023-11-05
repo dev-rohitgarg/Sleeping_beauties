d7152d4e498f3f3ebb08244a22c9bf0c59be9524
ATHER-101 <atharvatijare04@gmail.com>
2023-11-05 08:44:17 +0530

    forward backward login

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path("",views.index,name='home')
+    path("",views.index,name='home'),
+    path("login/",views.login,name='login'),
+    path("signin/",views.index,name='signin')
 ]
